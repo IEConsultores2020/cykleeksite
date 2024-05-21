665297cbdba8280471ef9b54836a513d6ff7f158
Fernando Torres Valencia <fernando.torres@ieconsultores.com>
2024-05-20 20:50:07 -0500

    minors changes, waiting the images.

--- assets/cykleek_js/readmore.js
+++ assets/cykleek_js/readmore.js
@@ -8,8 +8,9 @@ function readmorecommitment(dots, more, myBtn) {
         btnText.innerHTML = "Read more";
         moreText.style.display = "none";
     } else {
+        const svgCode = '<svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ><path d="M4 4V20M8 12H20M8 12L12 8M8 12L12 16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'
         dots.style.display = "none";
-        btnText.innerHTML = "Read less";
+        btnText.innerHTML = svgCode;
         moreText.style.display = "inline";
     }
 }
